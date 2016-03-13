10a8752ed2fe731b5f17ff5d6bf7215a78bfbf97
pmhealy <padraig.mark.healy@gmail.com>
2016-03-13 15:10:09 +0000

    Added files via upload

--- BasicDataManipulation.R
+++ BasicDataManipulation.R
@@ -13,7 +13,7 @@ str_trim(refine_df$company)
 company_cleaning<-str_trim(tolower(refine_df$company))
 
 ## Standardise mis-spelt Company names
-std_company <- c("philips", "akzo", "vanhouten","unilever")
+std_company <- c("philips", "akzo", "van houten","unilever")
 library(stringdist)
 (i <- amatch(company_cleaning,std_company,maxDist=3))
 
@@ -30,6 +30,7 @@ refine_df$company<-clean_company
 library(tidyr)
 refine_df<-separate(data = refine_df, col = Product.code...number, into = c("Product_Code", "Product_Number"), sep = "\\-")
 
+
 ## Step 3
 ## Add Product Categories
 oldvalues <- c("p", "v", "x", "q")
@@ -44,20 +45,25 @@ refine_df<-unite(refine_df,"Full Address", address, city, country, sep=",", remo
 refine_df
 
 
-##### Attempt
+##### Dummy Variables for Company
 
 library(dummies)
 company_dummy<-dummy(clean_company)
-colnames(company_dummy)[1] <- "company_philips"
-colnames(company_dummy)[2] <- "company_akzo"
-colnames(company_dummy)[3] <- "company_vanhouten"
-colnames(company_dummy)[4] <- "company_unilever"
+colnames(company_dummy)[1] <- "company_akzo"
+colnames(company_dummy)[2] <- "company_philips"
+colnames(company_dummy)[3] <- "company_unilever"
+colnames(company_dummy)[4] <- "company_van_houten"
+
+##### Dummy Variables for Product Category
 
 prod_dummy<-dummy(refine_df$Product_Category)
-colnames(prod_dummy)[1] <- "product_smartphone"
-colnames(prod_dummy)[2] <- "product_tv"
-colnames(prod_dummy)[3] <- "product_laptop"
-colnames(prod_dummy)[4] <- "product_tablet"
+colnames(prod_dummy)[1] <- "product_laptop"
+colnames(prod_dummy)[2] <- "product_smartphone"
+colnames(prod_dummy)[3] <- "product_tablet"
+colnames(prod_dummy)[4] <- "product_tv"
 
+## Combine the Data & the Dummy Variables
 refine_clean<-cbind(refine_df, company_dummy, prod_dummy)
+
+## Export in csv format
 write.csv(refine_clean, "C:/Users/Mark's/Desktop/Data Science/DataScienceAssignments/Basic Data Manipulation/refine_clean.csv")
